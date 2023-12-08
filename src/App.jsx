5804938cc25663bc12625b784d49db28cd373a73
mike focken <mikefocken@gmail.com>
2023-12-07 23:40:50 -0600

    save first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import NameBadge from './Components/NameBadge/NameBadge';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Mike Focken</h1>
+  </div>;
 }
 export default App;
